6acc71b80ee91ec5b3c1fc8c1938162cf9a64ed1
lluch2019087 <lluch-2019087@kinal.edu.gt>
2025-03-16 12:25:19 -0600

    Subiendo suma

---
+++ SUMA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class suma {
+	private : string numero1, operacion, numero2;
+		 
+		 	 
+	 
+		void agregar ();
+		 void mostrar ();
+		  void eliminar ();
+		   void actualizar ();
+		 
+};
